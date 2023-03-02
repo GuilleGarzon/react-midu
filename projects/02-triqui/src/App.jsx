7b20b0dcf86f1d402adc9748882101c22d3f3664
GuilleGarzon <guillegarzon.gg@gmail.com>
2023-03-02 18:39:40 -0500

    feat: Add components

--- projects/02-triqui/src/App.jsx
+++ projects/02-triqui/src/App.jsx
@@ -1,35 +1,9 @@
 import { useState } from "react";
 import confetti from "canvas-confetti";
-
-const TURNS = {
-  X: 'x',
-  O: 'o',
-};
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${ isSelected ? 'is-selected' : '' }`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={ handleClick } className= { className }>
-      { children }
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+import { Square } from "./components/Square";
+import { TURNS } from "./constants";
+import { checkWinnerFrom, checkEndGame } from "./logic/board";
+import { WinnerModal } from "./components/WinnerModal";
 
 function App() {
   const [board, setBoard] = useState(
@@ -38,33 +12,13 @@ function App() {
 
   const [turn, setTurn] = useState(TURNS.X)
   // null es que no hay ganador, false es que hay un empate
-  const [winner, setWinner] = useState(null) 
-
-  const checkWinner = ( boardToCheck ) => {
-    // revisamos todas las operaciones ganadoras para ver si X u O ganó
-    for ( const combo of WINNER_COMBOS ) {
-      const [a, b, c] = combo
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] && 
-        boardToCheck[a] === boardToCheck[c] 
-      ) {
-        return boardToCheck[a]
-      }
-    }
-    return null
-  }
+  const [winner, setWinner] = useState(null)   
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
-  }
-
-  const checkEndGame = (newBoard) => {
-    // revisamos si hay empate si no hay más espacios en el tablero
-    return newBoard.every((square) => square !== null)
-  }
+  } 
 
   const updateBoard = ( index ) => {
     // no actualizamos esta posición
@@ -78,7 +32,7 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn( newTurn );
     // revisar si hay ganador
-    const newWinner = checkWinner( newBoard )
+    const newWinner = checkWinnerFrom( newBoard );
     if ( newWinner ) {
       confetti();
       setWinner( newWinner );
@@ -114,31 +68,7 @@ function App() {
         </Square>
       </section>
 
-      {
-        winner !== null && (
-          <section className="winner">
-            <div className="text">
-              <h2>
-                {
-                  winner === false
-                  ? 'Empate'
-                  : 'Ganó: '
-                }
-              </h2>
-
-              <header className="win">
-                {
-                  winner && <Square>{ winner }</Square>
-                }
-              </header>
-
-              <footer>
-                <button onClick={resetGame}>Empezar de nuevo</button>
-              </footer>
-            </div>
-          </section>
-        )
-      }
+      <WinnerModal resetGame={ resetGame } winner={ winner } />      
 
     </main>
   );
---
+++ projects/02-triqui/src/logic/board.js
@@ -0,0 +1,21 @@
+import { WINNER_COMBOS } from "../constants";
+
+export const checkWinnerFrom = ( boardToCheck ) => {
+  // revisamos todas las operaciones ganadoras para ver si X u O ganó
+  for ( const combo of WINNER_COMBOS ) {
+    const [a, b, c] = combo
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] && 
+      boardToCheck[a] === boardToCheck[c] 
+    ) {
+      return boardToCheck[a]
+    }
+  }
+  return null
+}
+
+export const checkEndGame = (newBoard) => {
+  // revisamos si hay empate si no hay más espacios en el tablero
+  return newBoard.every((square) => square !== null)
+}
